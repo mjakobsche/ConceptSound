e7e7ac57934999879fe05b0427e3ef4c34d589dc
MateuszJakobsche <laaunair@protonmail.com>
2023-07-26 22:09:06 +0200

    fix: missing file in last commit

--- ConceptSound/src/stores/currentLibrary.ts
+++ ConceptSound/src/stores/currentLibrary.ts
@@ -1,23 +1,28 @@
 import { defineStore } from "pinia";
-import { ref, computed } from "vue";
+import { ref } from "vue";
 import storage from "@/data/Storage";
-
 export const useCurrentLibrary = defineStore("currentLibrary", () => {
 	const library = ref(storage);
-	const getBook = computed(() => {
-		return (id: number) => library.value[id];
-	});
+
 	function remBook(id: number) {
-		library.value = library.value.filter((t) => t.id !== id);
+		storage[id] = storage[storage.length - 1];
+		storage.pop();
 	}
 	function addBook(title: string) {
-		let bookIndex = library.value.length;
-		library.value.unshift({
+		let bookIndex = storage.length;
+		storage.unshift({
 			id: bookIndex,
 			title: title,
 			date: new Date(),
 			content: [],
 		});
 	}
-	return { library, getBook, remBook, addBook };
+
+	return { library, remBook, addBook };
 });
+//.map((book) => ({
+// 	id: book.id,
+// 	title: book.title,
+// 	date: book.date,
+// }))
+// .sort((a, b) => b.date.getTime() - a.date.getTime());
