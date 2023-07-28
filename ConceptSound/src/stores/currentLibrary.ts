e2fafacc7a27c68e4398afed52e74a10b0575908
MateuszJakobsche <laaunair@protonmail.com>
2023-07-28 18:06:02 +0200

    chore: setting course for db communication

--- ConceptSound/src/stores/currentLibrary.ts
+++ ConceptSound/src/stores/currentLibrary.ts
@@ -1,28 +1,21 @@
 import { defineStore } from "pinia";
 import { ref } from "vue";
 import storage from "@/data/Storage";
+import { getLibrary, addNewBook, getBookCover } from "@/data/LibraryService";
 export const useCurrentLibrary = defineStore("currentLibrary", () => {
-	const library = ref(storage);
-
+	const library = ref(getLibrary());
 	function remBook(id: number) {
 		storage[id] = storage[storage.length - 1];
 		storage.pop();
 	}
 	function addBook(title: string) {
-		let bookIndex = storage.length;
-		storage.unshift({
-			id: bookIndex,
-			title: title,
-			date: new Date(),
-			content: [],
-		});
+		let result = addNewBook(title);
+		let book = getBookCover(result.id);
+		if (!book) {
+			return;
+		}
+		library.value.unshift(book);
 	}
 
 	return { library, remBook, addBook };
 });
-//.map((book) => ({
-// 	id: book.id,
-// 	title: book.title,
-// 	date: book.date,
-// }))
-// .sort((a, b) => b.date.getTime() - a.date.getTime());
