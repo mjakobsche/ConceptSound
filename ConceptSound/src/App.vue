956f5ffc9867e98b0d62a13db1acda0c01d2764d
mjakobsche <m.jakobsche@proton.me>
2023-10-08 14:41:21 +0200

    final polish

--- ConceptSound/src/App.vue
+++ ConceptSound/src/App.vue
@@ -1,17 +1,15 @@
 <template>
   <suspense>
     <ion-app>
-      <ion-router-outlet/>
+      <ion-router-outlet />
     </ion-app>
   </suspense>
-
 </template>
 
 <script setup lang="ts">
-import {IonApp, IonRouterOutlet} from "@ionic/vue";
-import {getCurrentInstance} from 'vue';
-import {useSQLite} from 'vue-sqlite-hook';
-
+import { IonApp, IonRouterOutlet } from "@ionic/vue";
+import { getCurrentInstance } from 'vue';
+import { useSQLite } from 'vue-sqlite-hook';
 const app = getCurrentInstance();
 if (app != null) {
   app.appContext.config.globalProperties.$sqlite = useSQLite();
