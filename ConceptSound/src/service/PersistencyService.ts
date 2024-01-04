7965b421d4a82130b1f06d4c2e85fd47a8dd43a0
laaunair <laaunair@protonmail.com>
2024-01-04 11:52:22 +0100

    refactor: PersistencyService.ts

--- ConceptSound/src/service/PersistencyService.ts
+++ ConceptSound/src/service/PersistencyService.ts
@@ -1,60 +1,52 @@
 import {deleteFile, readDirectory, readFile, writeDirectory, writeFile} from "@/utils/FileSystemWrapper";
 import {BookCover} from "@/model/BookCover";
-import {FileInfo} from "@capacitor/filesystem";
 import {BookPage} from "@/model/BookPage";
 
-const fileDirectory: string = "library";
-const indexFile: string = "index";
-const fileExtension: string = ".json";
 
 async function persistBookPagesChanges(bookId: string, bookPages: BookPage[]): Promise<void> {
-    await writeFile(getFilePath(bookId), JSON.stringify(bookPages));
+    await writeFile("library/" + bookId + ".json", JSON.stringify(bookPages));
 }
 
 async function persistBooksChanges(bookCovers: BookCover[]): Promise<void> {
-    await writeFile(getFilePath(indexFile), JSON.stringify(bookCovers));
+    await writeFile("library/index.json", JSON.stringify(bookCovers));
     await assurePagesFilesIntegrity(bookCovers);
 }
 
 async function getPersistedBookPages(bookId: string): Promise<BookPage[]> {
-    return await readFileContents(bookId) as BookPage[];
+    return await readFileContents("library/" + bookId + ".json") as BookPage[];
 }
 
 async function getPersistedBooks(): Promise<BookCover[]> {
     let bookCovers: BookCover[];
     await initializeFileSystem();
-    bookCovers = await readFileContents(indexFile);
+    bookCovers = await readFileContents("library/index.json");
     return bookCovers;
 }
 
 async function assurePagesFilesIntegrity(bookCovers: BookCover[]): Promise<void> {
     const coversId: String[] = bookCovers.map((bookCover) => bookCover.id);
-    const pagesFiles: String[] = (await readBaseDirectory()).map((file) => file.name);
-    coversId.filter((coverId) => !pagesFiles.includes(coverId + fileExtension)).forEach((coverId) => writeFile(getFilePath(coverId), "[]"));
-    pagesFiles.map((pagesFile) => pagesFile.substring(0, pagesFile.length - 5)).filter((pagesFile) => !coversId.includes(pagesFile) && pagesFile !== indexFile).forEach((pagesFile) => deleteFile(getFilePath(pagesFile)));
+    const pagesFiles: String[] = await readDirectoryFiles("library")
+    coversId.filter((coverId) => !pagesFiles.includes(coverId + ".json")).forEach((coverId) => writeFile("library/" + coverId + ".json", "[]"));
+    pagesFiles.map((pagesFile) => pagesFile.substring(0, pagesFile.length - 5)).filter((pagesFile) => !coversId.includes(pagesFile) && pagesFile !== "index").forEach((pagesFile) => deleteFile("library/" + pagesFile + ".json"));
 }
 
 async function initializeFileSystem() {
-    let files = (await readDirectory("")).files.map((file) => file.name)
-    if (!files.includes(fileDirectory)) {
-        await writeDirectory(fileDirectory);
+    let files = await readDirectoryFiles("")
+    if (!files.includes("library")) {
+        await writeDirectory("library");
     }
-    files = (await readBaseDirectory()).map((file) => file.name)
-    if (!files.includes(indexFile + fileExtension)) {
-        await writeFile(getFilePath(indexFile), "[]");
+    files = await readDirectoryFiles("library")
+    if (!files.includes("index.json")) {
+        await writeFile("library/index.json", "[]");
     }
 }
 
-async function readBaseDirectory(): Promise<FileInfo[]> {
-    return (await readDirectory(fileDirectory)).files;
+async function readDirectoryFiles(directory: string): Promise<string[]> {
+    return (await readDirectory(directory)).files.map((file) => file.name);
 }
 
-async function readFileContents(fileName: string): Promise<any> {
-    return JSON.parse((await readFile(getFilePath(fileName))).data);
-}
-
-function getFilePath(fileName: string) {
-    return fileDirectory + "/" + fileName + fileExtension;
+async function readFileContents(filePath: string): Promise<any> {
+    return JSON.parse((await readFile(filePath)).data);
 }
 
 export {getPersistedBooks, persistBooksChanges, persistBookPagesChanges, getPersistedBookPages}
